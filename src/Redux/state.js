6e812701573df5be01a25db24cb02b71b64ca9b5
Kuznetsov <kuznetsov1398@gmail.com>
2022-03-23 11:30:35 +0700

    whitepage error

---
+++ src/Redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+    postsData: [
+        {id: 1, message: 'Hello!'},
+        {id: 2, message: 'Hi, gays, how are you?'},
+        {id: 3, message: 'I am fine!'}
+    ],
+
+    personsData: [
+        {id: 1, name: 'Паша'},
+        {id: 2, name: 'Вася'},
+        {id: 3, name: 'Оля'},
+        {id: 4, name: 'Дима'},
+        {id: 5, name: 'Витя'},
+        {id: 6, name: 'Коля'}
+    ],
+
+    messagesData: [
+        {id: 1, message: 'Hello!'},
+        {id: 2, message: 'Hi!'},
+        {id: 3, message: 'Привет!'}
+    ]
+}
+
+export default state
--- src/index.js
+++ src/index.js
@@ -4,34 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-
-let postsData = [
-    {id: 1, message: 'Hello!'},
-    {id: 2, message: 'Hi, gays, how are you?'},
-    {id: 3, message: 'I am fine!'}
-]
-
-let personsData = [
-    {id: 1, name: 'Паша'},
-    {id: 2, name: 'Вася'},
-    {id: 3, name: 'Оля'},
-    {id: 4, name: 'Дима'},
-    {id: 5, name: 'Витя'},
-    {id: 6, name: 'Коля'}
-]
-
-
-let messagesData = [
-    {id: 1, message: 'Hello!'},
-    {id: 2, message: 'Hi!'},
-    {id: 3, message: 'Привет!'}
-]
+import state from "./Redux/state";
 
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App postData={postsData} personsData={personsData} messagesData={messagesData}/>
+            <App state={state}/>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
